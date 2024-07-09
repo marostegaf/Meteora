7ce36b84eb3ea0391ed61ac22c32b6f8183b7e78
Marostega <marcosmarostega@hotmail.com>
2024-07-09 18:58:08 -0300

    v1.1

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Carrinho from "./pages/Carrinho";
+import PaginaErro from "./pages/PaginaErro";
+
+import "./App.css";
+import { CarrinhoProvider } from "./Context/CarrinhoContext";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <CarrinhoProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={ <Home /> }
+          />
+          <Route
+            path="/carrinho"
+            element= {<Carrinho /> }
+          />
+          <Route path="*" element={<PaginaErro />} />
+        </Routes>
+      </CarrinhoProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
